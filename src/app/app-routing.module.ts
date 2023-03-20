97d0de258e3aaab42dae7a70036dc058c4ace97a
olehsemkiv <olegsemkiv3065@gmail.com>
2023-03-20 15:21:44 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { ProductComponent } from './pages/product/product.component';
+import { ProductInfoComponent } from './pages/product-info/product-info.component';
+import { BlogComponent } from './pages/blog/blog.component';
+import { ContactsComponent } from './pages/contacts/contacts.component';
+import { DeliveryComponent } from './pages/delivery/delivery.component';
+import { CheckoutComponent } from './pages/checkout/checkout.component';
+import { AboutComponent } from './pages/about/about.component';
+import { BlogInfoComponent } from './pages/blog-info/blog-info.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'product', component: ProductComponent },
+  { path: 'product-info', component: ProductInfoComponent },
+  { path: 'blog', component: BlogComponent },
+  { path: 'contacts', component: ContactsComponent },
+  { path: 'delivery', component: DeliveryComponent },
+  { path: 'checkout', component: CheckoutComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'blog-info', component: BlogInfoComponent },
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProductComponent } from './pages/product/product.component';
+import { ProductInfoComponent } from './pages/product-info/product-info.component';
+import { BlogComponent } from './pages/blog/blog.component';
+import { ContactsComponent } from './pages/contacts/contacts.component';
+import { DeliveryComponent } from './pages/delivery/delivery.component';
+import { CheckoutComponent } from './pages/checkout/checkout.component';
+import { AboutComponent } from './pages/about/about.component';
+import { BlogInfoComponent } from './pages/blog-info/blog-info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ProductComponent,
+    ProductInfoComponent,
+    BlogComponent,
+    ContactsComponent,
+    DeliveryComponent,
+    CheckoutComponent,
+    AboutComponent,
+    BlogInfoComponent
   ],
   imports: [
     BrowserModule,
